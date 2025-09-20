5c8e5acc1f04cd6f90a5fd0e137287f24d81d71b
zhaniya05 <baitemirovazh05@gmail.com>
2025-09-20 14:55:38 +0500

    Initial commit

---
+++ src/main/java/com/example/pethelper/PetHelperApplication.java
@@ -0,0 +1,13 @@
+package com.example.pethelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetHelperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pethelper/PetHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pethelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
