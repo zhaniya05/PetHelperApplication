bbe0d60af10a3a1b1f402b77c6b531ce7c859112
zhaniya05 <baitemirovazh05@gmail.com>
2025-09-27 15:57:20 +0500

    Correction of pages

--- src/main/java/com/example/pethelper/repository/PetRepository.java
+++ src/main/java/com/example/pethelper/repository/PetRepository.java
@@ -3,6 +3,10 @@ package com.example.pethelper.repository;
 import com.example.pethelper.entity.Pet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PetRepository extends JpaRepository<Pet, Long> {
+    List<Pet> findByUserUserId(Long userId);
+
 
 }
--- src/main/java/com/example/pethelper/security/SecurityConfig.java
+++ src/main/java/com/example/pethelper/security/SecurityConfig.java
@@ -3,10 +3,14 @@ package com.example.pethelper.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 public class SecurityConfig {
@@ -15,22 +19,26 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
+                //.csrf().disable()
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/register", "/auth/login", "/css/**", "/js/**").permitAll()
+                        .requestMatchers("/api/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(login -> login
-                        .loginPage("/login")
+                        .loginPage("/auth/login")
+                         .defaultSuccessUrl("/pets/main", true)
                         .permitAll()
                 )
                 .logout(logout -> logout
                         .logoutUrl("/auth/logout")
                         .permitAll()
+
+
                 );
 
         return http.build();
